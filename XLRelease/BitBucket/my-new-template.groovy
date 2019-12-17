6a2275c6792ca91e1c4b42e01dd3145648ce2ee6
Rob Vanstone <rvanstone@xebialabs.com>
2019-12-17 15:52:59 +0000

    Initial Version

---
+++ XLRelease/BitBucket/my-new-template.groovy
@@ -0,0 +1,16 @@
+// Exported from:        http://Robs-MacBook-Pro-3.local:6516/#/templates/Folder90c8ba97eaf64996922a1d9fc83a16b2-Release4f48d87b28984112b63e18602011ea6e/releasefile
+// XL Release version:   9.5.0
+// Date created:         Tue Dec 17 15:52:58 GMT 2019
+
+xlr {
+  template('my-new-template') {
+    folder('BitBucket')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2019-12-17T09:00:00+0000')
+    phases {
+      phase('New Phase') {
+        
+      }
+    }
+    
+  }
+}
